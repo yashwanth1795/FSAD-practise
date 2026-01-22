e2b4af297392efa1d33319b21fa8f2b2bcaa11ad
Gowni Yashwanth <gowniyashwanth72@gmail.com>
2026-01-22 10:32:35 +0530

    22-01-2026

---
+++ src/main/java/com/klu/DiECommerceAppAutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.klu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiECommerceAppAutowiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiECommerceAppAutowiredApplication.class, args);
+	}
+
+}
