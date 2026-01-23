1a479bc7d34e2adbdab40193186e48770fa83b02
2400032326@kluniversity.in <2400032326@kluniversity.in>
2026-01-23 08:28:59 +0530

    Date: 22-01-2026

--- Springdemo/src/main/java/com/klu/Bike.java
+++
@@ -1,7 +0,0 @@
-package com.klu;
-
-public class Bike {
-    public void ride() {
-        System.out.println("Bike is riding...");
-    }
-}
--- Springdemo/src/main/java/com/klu/Engine.java
+++
@@ -1,7 +0,0 @@
-package com.klu;
-
-public class Engine {
-    public void Start() {
-        System.out.println("Engine started... Vroom!");
-    }
-}
